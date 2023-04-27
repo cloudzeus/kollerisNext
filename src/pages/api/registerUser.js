79205d2c41ace7994ad6601bd37cdd6cf973703e
giannischiout <giannis.chiout@gmail.com>
2023-04-27 17:15:43 +0300

    regex email password

--- src/pages/api/registerUser.js
+++ src/pages/api/registerUser.js
@@ -4,10 +4,22 @@ import connectMongo from "../../../server/utils/connection";
 import User from "../../../server/models/userModel";
 
 // 
+let regexEmail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+let regexPass = new RegExp('/^(?=.*[A-Z]).{8,}$/');
+
+
 
 export default async function handler(req, res) {
   try {
     await connectMongo();
+    let password = req.body.password;
+    let email = req.body.email;
+    if(!regexEmail.test(email)) {
+      res.status(200).json({  error: 'invalid email' })
+    }
+    if(!regexPass.test(password)) {
+      res.status(200).json({  error: 'invalid email' })
+    }
     const user = await User.create(req.body);
     res.status(200).json({  user })
   } catch (err) {
