29c442bfd765b6b585d0f43a6395d448b50b5906
giannischiout <giannis.chiout@gmail.com>
2023-05-11 09:43:18 +0300

    downgrade syncfusion

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -5,9 +5,9 @@ import { toast } from 'react-toastify';
 
 
 const initialState = {
-  user: getUserFromLocalStorage(),
-  isLoading: false,
-  isSidebarOpen: true,
+	user: getUserFromLocalStorage(),
+	isLoading: false,
+	isSidebarOpen: true,
 }
 
 
@@ -15,121 +15,120 @@ const initialState = {
 
 
 export const fetchUser = createAsyncThunk(
-  //action:
-  'user/fetchUser',
-  async (user, thunkApi) => {
-    console.log(`Fetch User: ${JSON.stringify(user)}`)
-    try {
-      const resp = await axios.post('/api/user/fetchuser', user)
-      return resp.data;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error.response.data)
-    }
-  })
+	//action:
+	'user/fetchUser',
+	async (user, thunkApi) => {
+		console.log(`Fetch User: ${JSON.stringify(user)}`)
+		try {
+			const resp = await axios.post('/api/user/fetchuser', user)
+			return resp.data;
+		} catch (error) {
+			return thunkApi.rejectWithValue(error.response.data)
+		}
+	})
 
 
 //REGISTER USER:
 export const registerUser = createAsyncThunk(
-  //action:
-  'user/registerUser',
-  async (user, thunkApi) => {
-    console.log(`Register User: ${JSON.stringify(user)}`)
-    try {
-      const resp = await axios.post('/api/user/registeruser', user)
-      return resp.data;
+	//action:
+	'user/registerUser',
+	async (user, thunkApi) => {
+		console.log(`Register User: ${JSON.stringify(user)}`)
+		try {
+			const resp = await axios.post('/api/user/registeruser', {
 
-    } catch (error) {
-      console.log(error)
-    }
-  })
+			}, user)
+			return resp.data;
+
+		} catch (error) {
+			console.log(error)
+		}
+	})
 
 //UPDATE USER:
 export const updateUser = createAsyncThunk(
-  //action:
-  'user/updateUser',
-  async (user, thunkApi) => {
-    console.log(`Update User: ${JSON.stringify(user)}`)
-    try {
-      const resp = await axios.post('/api/user/update', user)
-      return resp.data;
+	//action:
+	'user/updateUser',
+	async (user, thunkApi) => {
+		console.log(`Update User: ${JSON.stringify(user)}`)
+		try {
+			const resp = await axios.post('/api/user/update', user)
+			return resp.data;
 
-    } catch (error) {
-      console.log(error)
-    }
-  })
+		} catch (error) {
+			console.log(error)
+		}
+	})
 
 
 const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    toggleSidebar: (state) => {
-      state.isSidebarOpen = !state.isSidebarOpen;
-    },
-    logoutUser: (state) => {
-      state.user = null;
-      state.isLoading = false;
-      removerFromLocalStorage();
-    },
-    
+	name: 'user',
+	initialState,
+	reducers: {
+		toggleSidebar: (state) => {
+			state.isSidebarOpen = !state.isSidebarOpen;
+		},
+		logoutUser: (state) => {
+			state.user = null;
+			state.isLoading = false;
+			removerFromLocalStorage();
+		},
 
 
-  },
-  extraReducers: (builder) => {
-    builder
-      // LOGIN USER:
-      .addCase(fetchUser.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(fetchUser.fulfilled, (state, { payload }) => {
-        const { user } = payload;
-        console.log('User in reducers' + JSON.stringify(user))
-       
-        state.user = user;
-        if (user) {
-          addUserToLocalStorage(user)
-        }
-        state.isLoading = false;
-      })
-      .addCase(fetchUser.rejected, (state, { payload }) => {
-        state.isLoading = false;
 
-      })
-      //REGISTER USER:
-      .addCase(registerUser.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(registerUser.fulfilled, (state,  {payload} ) => {
-        // const { user } = payload;
-        console.log('Payload in register user: ' + JSON.stringify(payload))
-        // console.log('are we here')
-        // console.log(payload)
-        // state.isLoading = false;
-        // state.user = user;
-        // addUserToLocalStorage(user)
-        if(payload?.success == false && payload?.error) {
-          toast.error(payload.error)
-        }
-        
-      })
-      .addCase(registerUser.rejected, (state, { payload }) => {
-        state.isLoading = false;
-      })
-      //UPDATE USER:
-      .addCase(updateUser.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(updateUser.fulfilled, (state, { payload }) => {
-        console.log(payload)
-        const { user } = payload;
-        state.user = user;
-        state.isLoading = false;
-        addUserToLocalStorage(user)
-      })
-      .addCase(updateUser.rejected, (state, { payload }) => {
-        state.isLoading = false;
-      })
-  }
+	},
+	extraReducers: (builder) => {
+		builder
+			// LOGIN USER:
+			.addCase(fetchUser.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(fetchUser.fulfilled, (state, { payload }) => {
+				const { user } = payload;
+				console.log('User in reducers' + JSON.stringify(user))
+
+				state.user = user;
+				if (user) {
+					addUserToLocalStorage(user)
+				}
+				state.isLoading = false;
+			})
+			.addCase(fetchUser.rejected, (state, { payload }) => {
+				state.isLoading = false;
+
+			})
+			//REGISTER USER:
+			.addCase(registerUser.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(registerUser.fulfilled, (state, { payload }) => {
+				const { user } = payload;
+				state.isLoading = false;
+				state.user = user;
+				addUserToLocalStorage(user)
+				// if(payload?.success == false && payload?.error) {
+				//   toast.error(payload.error)
+				// }
+
+			})
+			.addCase(registerUser.rejected, (state, { payload }) => {
+				state.isLoading = false;
+			})
+			//UPDATE USER:
+			.addCase(updateUser.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(updateUser.fulfilled, (state, { payload }) => {
+				console.log(payload)
+				const { user } = payload;
+				state.user = user;
+				state.isLoading = false;
+				addUserToLocalStorage(user)
+			})
+			.addCase(updateUser.rejected, (state, { payload }) => {
+				state.isLoading = false;
+			})
+	}
 })
 
 
