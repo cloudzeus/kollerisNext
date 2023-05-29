6b2032fb600500efda62073d3321928e247f868c
johnchiout <125861274+johnchiout@users.noreply.github.com>
2023-05-29 11:37:28 +0300

    Create styles.js

---
+++ src/components/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+//Styled DIVs:
+
+
+
+const BasicContainer = styled.div`
+    /* box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px; */
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+    background-color: white;
+    border-radius: 4px;
+`
+
+export {
+  BasicContainer
+}
