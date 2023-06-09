5b7ecbd167d546812380389e6805881d3a8eb2ba
giannischiout <giannis.chiout@gmail.com>
2023-06-09 11:39:34 +0300

    testProd

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const connectMongo = async () => mongoose.connect(process.env.MONGO_URI).then(() => console.log('Mongo Connected!'));
+const connectMongo = async () => mongoose.connect("mongodb://dgsoft:Kozyris72541969@kolleris.mbmv.io:5652/kolleris?authSource=admin").then(() => console.log('Mongo Connected!'));
 
 export default connectMongo;
 
