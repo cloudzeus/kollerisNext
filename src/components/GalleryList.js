1698fb0dacd0718fd5124594fb278afc1b5a0f08
giannischiout <giannis.chiout@gmail.com>
2023-06-26 10:41:13 +0300

    image gallery slideshow

--- src/components/GalleryList.js
+++ src/components/GalleryList.js
@@ -1,21 +1,176 @@
-import Image from "next/image";
-const GalleryList = ({images}) => {
+import { useState } from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+
+import { Button } from 'primereact/button';
+
+
+
+
+const GalleryContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+const LargeImageContainer = styled.div`
+    position: relative;
+
+`;
+const LargeImage = styled.div`
+  width: 30%;
+  /* flex: 0 0 auto; */
+  width: 300px;
+  height: 300px;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+  img {
+    object-fit: cover;
+  }
+
+`;
+
+const ThumbnailContainer = styled.div`
+  width: 70%;
+  /* flex: 0 0 auto; */
+  overflow-y: auto;
+  height: 300px;
+  padding: 0 10px;
+`;
+
+const ThumbnailGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  align-items: center;
+  justify-items: center;
+  grid-gap: 5px;
+
+
+`;
+
+const Thumbnail = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  border-radius: 4px;
+  width: 100%;
+  height: 100px;
+  overflow: hidden;
+  opacity: ${(props) => (props.isSelected ? 1 : 0.5)};
+
+  img {
+    object-fit: cover;
+  }
+
+  button {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+  }
+`;
+
+const ArrowContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    .p-button  {
+        background-color: #fff;
+        opacity: 70%;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    } 
+    .p-button:enabled:hover, .p-button:not(button):not(a):not(.p-disabled):hover {
+        background-color: #fff;
+        opacity: 70%;
+    } 
+    
+`;
+
+
+const images = [
+    'addidas1.jpg',
+    'addidas2.jpg',
+    'addidas3.jpg',
+    'addidas4.jpg',
+    'addidas5.jpg',
+
+
+
+    // Add more image paths here
+];
+
+const Gallery = () => {
+    const [selectedImage, setSelectedImage] = useState(images[0]);
+
+    const handleImageSelect = (image) => {
+        setSelectedImage(image);
+    };
+
+    const handleDeleteImage = (image) => {
+        // Implement your delete logic here
+        console.log(`Deleting image: ${image}`);
+    };
+
+    const handlePrevImage = () => {
+        const currentIndex = images.indexOf(selectedImage);
+        const prevIndex = (currentIndex - 1 + images.length) % images.length;
+        setSelectedImage(images[prevIndex]);
+    };
+
+    const handleNextImage = () => {
+        const currentIndex = images.indexOf(selectedImage);
+        const nextIndex = (currentIndex + 1) % images.length;
+        setSelectedImage(images[nextIndex]);
+    };
+
     return (
-        <div>
-            <h1>GalleryList</h1>
-            {images.map((image, index) => {
-                return (
-                    <ImageDiv key={index}>
-                    </ImageDiv>
-                )
-            })}
-        </div>
-    )
-}
+        <GalleryContainer>
+            <LargeImageContainer>
+            <LargeImage>
+                <Image src={`/uploads/${selectedImage}`} alt="Large" fill={true} />
+            </LargeImage>
+            <ArrowContainer>
+                {images.length > 1 && (
+                    <>
+                        {selectedImage !== images[0] && (
+                            <Button onClick={handlePrevImage} icon="pi pi-angle-left" rounded outlined  aria-label="Favorite" />
+                        )}
+                        {selectedImage !== images[images.length - 1] && (
+                            <Button onClick={handleNextImage} icon="pi pi-angle-right" rounded outlined  aria-label="Favorite" />
+                        )}
+                    </>
+                )}
+                </ArrowContainer>
+            </LargeImageContainer>
+            <ThumbnailContainer>
+                <ThumbnailGrid>
+                    {images.map((image, index) => (
+                        <Thumbnail key={index} isSelected={image === selectedImage}>
+                            <Image
+                                src={`/uploads/${image}`}
+                                alt={`Thumbnail ${index}`}
+                                fill={true}
+                                onClick={() => handleImageSelect(image)} />
+                            {/* <button onClick={() => handleDeleteImage(image)}>
+                                <i className="pi pi-times" style={{ fontSize: '1.5rem' }}></i>
+                            </button> */}
+                        </Thumbnail>
+                    ))}
+                </ThumbnailGrid>
+            </ThumbnailContainer>
+        </GalleryContainer>
+    );
+};
 
-const ImageDiv = styled.div`
-    width: 60px;
-    height: 60px;
-`
 
-export default GalleryList;
+
+
+
+
+export default Gallery;
--- src/pages/dashboard/testgrid.js
+++ src/pages/dashboard/testgrid.js
@@ -11,26 +11,22 @@ import { Toast } from 'primereact/toast'
 import { FilterMatchMode, FilterOperator } from 'primereact/api';
 import { InputText } from 'primereact/inputtext';
 import { Toolbar } from 'primereact/toolbar';
-import { ImageInput } from '@/components/Forms/newInputs/ImageInput';
 import BrandDialog from '@/GridDialogs/brandDialog';
-import { useDispatch } from 'react-redux';
-import { setBrandDialog } from '@/features/brand/brandSlice';
-import { Galleria } from 'primereact/galleria';
-import GalleryImages from '@/components/Gallery';
+import Gallery from '@/components/GalleryList';
 import Link from 'next/link'
 
-
+import { TabView, TabPanel } from 'primereact/tabview';
 
 export default function TemplateDemo() {
     const [editData, setEditData] = useState([]);
     const [dialog, setDialog] = useState(false);
-    
+
     const [submitted, setSubmitted] = useState(false);
     const [data, setData] = useState([])
     const [expandedRows, setExpandedRows] = useState(null);
     const [filters, setFilters] = useState({
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-       
+
     });
 
     //
@@ -39,7 +35,7 @@ export default function TemplateDemo() {
     const handleFetchUser = async () => {
         try {
             const resp = await axios.post('/api/admin/markes/markes', { action: 'findAll' })
-           
+
             setData(resp.data.markes)
             setImages(resp.data.images)
 
@@ -50,12 +46,12 @@ export default function TemplateDemo() {
 
 
     useEffect(() => {
-         handleFetchUser();
-      
+        handleFetchUser();
+
     }, []);
 
- 
-   
+
+
 
     const logoTemplate = (data) => {
         let logo = data.logo ? data.logo : 'notfound.jpg'
@@ -73,7 +69,7 @@ export default function TemplateDemo() {
         )
     }
     //TEMPLATES
- 
+
     const renderHeader = () => {
         const value = filters['global'] ? filters['global'].value : '';
 
@@ -95,8 +91,8 @@ export default function TemplateDemo() {
         setFilters(_filters);
     };
 
-    
-    
+
+
     const allowExpansion = (rowData) => {
         // 
         return rowData
@@ -110,10 +106,10 @@ export default function TemplateDemo() {
     };
 
     const rowExpansionTemplate = (data) => {
-   
+
         return (
             < ShowDetails >
-                <div className="list-item">
+                {/* <div className="list-item">
                     <span>Ιστοσελίδα:</span>
                     <Link className="grid-link" href={data.webSiteUrl}>{data.webSiteUrl}</Link>
                 </div>
@@ -130,26 +126,31 @@ export default function TemplateDemo() {
                 <div className="divider"></div>
                 <div className="list-item">
                     <span>Εικόνες:</span>
-                    < GalleryImages images={images} />
-                </div>
-                
-                {/* <DataTable 
-                    value={data.orders}
-                    expandedRows={expandedRows} 
-                    onRowToggle={(e) => setExpandedRows(e.data)}
-                    onRowExpand={onRowExpand}
-                    onRowCollapse={onRowCollapse}
-                    rowExpansionTemplate={rowExpansionTemplate}
-                    dataKey="id"
-                
-                >
-                    <Column field="id" header="Id" sortable></Column>
-                    <Column field="customer" header="Customer" sortable></Column>
-                    <Column field="date" header="Date" sortable></Column>
-                    <Column field="amount" header="Amount" body={amountBodyTemplate} sortable></Column>
-                    <Column field="status" header="Status" body={statusOrderBodyTemplate} sortable></Column>
-                    <Column headerStyle={{ width: '4rem' }} body={searchBodyTemplate}></Column>
-                </DataTable> */}
+                </div> */}
+                    <div className="card">
+            <TabView>
+                <TabPanel header="Header I">
+                    <Gallery  />
+                </TabPanel>
+                <TabPanel header="Header II">
+                    <p className="m-0">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+                        eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                        enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui 
+                        ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                    </p>
+                </TabPanel>
+                <TabPanel header="Header III">
+                    <p className="m-0">
+                        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti 
+                        quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in
+                        culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
+                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+                    </p>
+                </TabPanel>
+                <TabPanel header="Header IV" disabled></TabPanel>
+            </TabView>
+        </div>
             </ ShowDetails >
         );
     };
@@ -170,15 +171,15 @@ export default function TemplateDemo() {
         );
     };
 
-  
+
     //Edit:
     const editProduct = (product) => {
-       
+
         setEditData({ ...product })
         setDialog(true)
     };
 
-   
+
 
     const hideDialog = () => {
         setSubmitted(false);
@@ -206,7 +207,7 @@ export default function TemplateDemo() {
         //     setProducts(_products);
         //     setProductDialog(false);
         //     setProduct(emptyProduct);
-        }
+    }
 
     // CUSTOM TEMPLATES FOR COLUMNS
     const actionBodyTemplate = (rowData) => {
@@ -219,17 +220,17 @@ export default function TemplateDemo() {
     };
     return (
         <AdminLayout >
-             {/* <Toast ref={toast} /> */}
-             <Toolbar className="mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
-            <DataTable 
+            {/* <Toast ref={toast} /> */}
+            <Toolbar className="mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
+            <DataTable
                 header={header}
-                value={data} 
-                paginator 
-                rows={8} 
-                rowsPerPageOptions={[5, 10, 25, 50]} 
+                value={data}
+                paginator
+                rows={8}
+                rowsPerPageOptions={[5, 10, 25, 50]}
                 showGridlines
-                rowExpansionTemplate = {rowExpansionTemplate}
-                expandedRows={expandedRows} 
+                rowExpansionTemplate={rowExpansionTemplate}
+                expandedRows={expandedRows}
                 onRowToggle={(e) => setExpandedRows(e.data)}
                 onRowExpand={onRowExpand}
                 onRowCollapse={onRowCollapse}
@@ -237,33 +238,33 @@ export default function TemplateDemo() {
                 filters={filters} onFilter={(e) => setFilters(e.filters)}
                 //edit:
                 editMode="row"
-            >      
+            >
                 {/* <Column field="softOne.MTRMARK" header="MTRMARK" sortable></Column> */}
-                <Column bodyStyle={{ textAlign: 'center' }}  expander={allowExpansion} style={{ width: '20px' }} />
+                <Column bodyStyle={{ textAlign: 'center' }} expander={allowExpansion} style={{ width: '20px' }} />
                 <Column field="logo" header="Λογότυπο" body={logoTemplate} ></Column>
                 <Column field="softOne.NAME" header="Ονομα" sortable></Column>
-                <Column field="description" header="Περιγραφή" tableStyle={{ width: 'auto'}}></Column>
-                <Column field="softOne.ISACTIVE" header="Status" tableStyle={{ width: '100px'}} body={ActiveTempate}></Column>
+                <Column field="description" header="Περιγραφή" tableStyle={{ width: 'auto' }}></Column>
+                <Column field="softOne.ISACTIVE" header="Status" tableStyle={{ width: '100px' }} body={ActiveTempate}></Column>
                 {/* <Column header="Actions"  body={actionsTemplate} tableStyle={{ width: '80px'}}></Column> */}
                 {/* <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column> */}
-                <Column body={actionBodyTemplate} exportable={false}  bodyStyle={{ textAlign: 'center' }} style={{ minWidth: '12rem' }}></Column>
+                <Column body={actionBodyTemplate} exportable={false} bodyStyle={{ textAlign: 'center' }} style={{ minWidth: '12rem' }}></Column>
 
             </DataTable>
-            <BrandDialog 
-                data={editData} 
-                dialog={dialog} 
+            <BrandDialog
+                data={editData}
+                dialog={dialog}
                 setDialog={setDialog}
                 hideDialog={hideDialog}
                 saveProduct={saveProduct}
                 submitted={submitted}
                 setSubmitted={setSubmitted}
-                />
+            />
         </AdminLayout >
     );
 }
 
 
-const ActiveTempate = ({softOne}) => {
+const ActiveTempate = ({ softOne }) => {
     const isActive = softOne.ISACTIVE
     return (
         <div>
@@ -272,7 +273,7 @@ const ActiveTempate = ({softOne}) => {
             ) : (
                 <Tag severity="Danger" value="deleted" ></Tag>
             )}
-            
+
         </div>
     )
 
@@ -318,9 +319,7 @@ const ActionDiv = styled.span`
 
 const ShowDetails = styled.div`
     border: 1px solid #e0e0e0;
-    div {
-        padding: 20px;
-    }
+   
     .list-item  {
         padding: 20px;
         /* border-bottom: 1px solid #e0e0e0; */
