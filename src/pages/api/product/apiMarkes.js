89255e040cadd990c720dff08fcc95cc1ee3aa78
giannischiout <giannis.chiout@gmail.com>
2023-07-20 14:05:29 +0300

    removed unuded code complete

--- src/pages/api/product/apiMarkes.js
+++ src/pages/api/product/apiMarkes.js
@@ -239,68 +239,6 @@ export default async function handler(req, res) {
 			return res.status(500).json({ success: false, error: 'Aποτυχία εισαγωγής', result: null});
 		}
 	}
-
-	
-	
-	if(action == 'addImages') {
-		console.log('add new Images to the database')
-		let id = req.body.id;
-		let images = req.body.images;
-		console.log(id, images)
-		let newArray = [];
-		//construc images object
-		for(let image of images) {
-			let obj = {
-				name: image,
-				photosPromoUrl: image
-			}
-			newArray.push(obj)
-		}
-		// Images must be and array
-		try {
-			await connectMongo();
-			let response = await Markes.updateOne({ _id: id }, 
-				{ $push: { photosPromoList: { $each: [... newArray] } } }
-			);
-			// console.log(response)
-			return res.status(200).json({ success: true, message:'Έγινε update', error: null });
-		} catch (e) {
-			return res.status(500).json({ success: false, error:'Δεν έγινε update', message: null });
-		}
-	}
-	if(action === 'deleteImages') {
-		console.log('delete Images 2')
-		let id = req.body.id;
-		let image = req.body.image;
-		console.log(id, image)
-		try {
-			await connectMongo();
-			let resp = await Markes.updateOne({ _id: id }, 
-			{ $pull: { photosPromoList: {
-				name: image,
-				photosPromoUrl: image
-			 }}}
-			);
-			console.log(resp)
-			return res.status(200).json({ success: true, message:'Έγινε update', error: null, result: resp });
-		} catch (e) {
-			return res.status(500).json({ success: false, error:'Δεν έγινε update', message: null, result: null });
-		}
-	}
-
-	if(action === 'deleteLogo') {
-		let {id} = req.body;
-		try {
-			await connectMongo();
-			let resp = await Markes.updateOne({ _id: id }, 
-			{ $set: { logo: ''} }
-			);
-			console.log(resp)
-			return res.status(200).json({ success: true, message:'Έγινε update στο λογότυπο', error: null, result: resp });
-		} catch (e) {
-			return res.status(500).json({ success: false, error:'Δεν έγινε update στο λογότυπο', message: null, result: null });
-		}
-	}
 }
 
 
--- src/store.js
+++ src/store.js
@@ -1,14 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
 import gridSlice from "./features/grid/gridSlice";
-import markesNotFoundSoftone from "./features/syncProduct/markesNotFoundSoftone";
-import markesNotFoundAriadne from "./features/syncProduct/markesNotFoundAriadne";
+
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     grid: gridSlice,
-    notFoundAriadne: markesNotFoundAriadne,
-    notFoundSoftone: markesNotFoundSoftone ,
   }
 })
