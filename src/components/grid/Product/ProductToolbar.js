bbc4b3410fd2b939fe883206d85d14e9c0e56fb6
giannischiout <giannis.chiout@gmail.com>
2023-09-07 09:50:51 +0300

    basket to toolbar

--- src/components/grid/Product/ProductToolbar.js
+++ src/components/grid/Product/ProductToolbar.js
@@ -13,50 +13,71 @@ import MultiDelete from './MultiDelete';
 import MultiImpa from './MultiImpa';
 import { Dropdown } from 'primereact/dropdown';
 import ToolbarActions from './ToolbarActions';
+import { Badge } from 'primereact/badge';
 
 
-
-const MenuOptions = ({ setId, setClickMenu }) => {
-    const onClick = (id) => {
-        setClickMenu(false)
-        setId(id)
-        console.log(id)
-    }
-
-    return (
-        <MenuDiv>
-            <ul>
-                <li onClick={() => onClick(1)}>Αλλαγή κατηγοριοποίησης</li>
-                <div className='line'></div>
-                <li onClick={() => onClick(2)}>Αλλαγή Impa</li>
-            </ul>
-        </MenuDiv>
-    )
-}
 
 
 const ProductToolbar = ({ selectedProducts, setSelectedProducts, setSubmitted }) => {
 
+    console.log('selected products')
+    console.log(selectedProducts)
 
     const startContent = () => {
         return <MenuComp selectedProducts={selectedProducts}  setSelectedProducts={setSelectedProducts} setSubmitted={setSubmitted}/>
     }
 
+    const endContent = () => {
+        return <RightSide selectedProducts={selectedProducts}  setSelectedProducts={setSelectedProducts} setSubmitted={setSubmitted}/>
+    }
+
     return (
         <div>
-            <Toolbar start={startContent} />
+            <Toolbar start={startContent} end={endContent}/>
         </div>
     )
 }
 
 
+const RightSide = ({ selectedProducts}) => {
+
+    const CalculateBasket = () => {
+        let total = 0
+        selectedProducts && selectedProducts.forEach((item) => {
+            total += parseInt(item.PRICER)
+        })
+        return (
+            <p className='mr-3'>Σύνολο:<span className='font-bold ml-1'>{`${total},00$`}</span> </p>
+        )
+
+
+    }
+    return (
+        <ContainerBasket >
+        <div className='basket-icon'>
+        < CalculateBasket />
+            <i onClick={() => setVisible(true)} className="pi pi-shopping-cart p-overlay-badge" style={{ fontSize: '25px', marginRight: '10px' }}>
+                <Badge value={selectedProducts == null ? "0" :selectedProducts?.length} ></Badge>
+            </i>
+        </div>
+    </ContainerBasket >
+    )
+}
+
 
 const MenuComp = ({ selectedProducts, setSelectedProducts, setSubmitted }) => {
-    const [clickMenu, setClickMenu] = useState(false)
-    const [id, setId] = useState(null);
     const [visible, setVisible] = useState(false)
-    const [products, setProducts] = useState(null)
+    const CalculateBasket = () => {
+        let total = 0
+        selectedProducts && selectedProducts.forEach((item) => {
+            total += parseInt(item.PRICER)
+        })
+        return (
+            <p className='mr-3 ml-1'><span className='font-normal'>Σύνολο:</span> {`${total},00$`}</p>
+        )
 
+
+    }
 
     const onMultipleActions = () => {
         setVisible(true)
@@ -82,16 +103,26 @@ const MenuComp = ({ selectedProducts, setSelectedProducts, setSubmitted }) => {
 
     const footer = () => {
         return (
-            <>
-                <p>{`Σύνολο Προϊόντων: ${products !== null ? products.length : 0}`}</p>
-            </>
+            <div className='flex justify-between p-2'>
+                <p className='mr-3 '><span className='font-normal'>Προϊόντα:</span> {`${selectedProducts !== null ? selectedProducts.length : 0}`}</p>
+                <CalculateBasket />
+            </div>
         )
     }
 
     return (
         <>
 
-            <Button icon="pi pi-align-right" label="Επεξεργασία όλων " className="mr-2" onClick={onMultipleActions} aria-controls="popup_menu_right" aria-haspopup />
+            <Button 
+                icon="pi pi-shopping-cart" 
+                label="Kαλάθι λειτουργειών" 
+                className="mr-2" 
+                onClick={onMultipleActions} 
+                aria-controls="popup_menu_right"
+                 aria-haspopup 
+                 severity="warning"
+                 disabled={selectedProducts == null || selectedProducts.length < 1 ? true : false}
+                 />
             <Sidebar visible={visible} position="right" onHide={() => setVisible(false)} className="md:w-6	 lg:w-5	">
                 <div className='flex align-items-center mb-2 mt-2'>
                     <h2>Επεξεργασία:</h2>
@@ -124,26 +155,53 @@ const MenuComp = ({ selectedProducts, setSelectedProducts, setSubmitted }) => {
 
 
 
+
 const ProductBaksetTemplate = ({ name, PRICER, categoryName, }) => {
     return (
-        <ProductGrid>
+        <ProductBasket>
             <div>
                 <p className='text-md text-900 font-semibold	'>{name}</p>
             </div>
             <div className='details'>
                 <div>
-                    <i className="pi pi-tag" style={{ fontSize: '12px', marginRight: '3px', marginTop: '2px' }}></i>
+                    <i className="pi pi-tag" style={{ fontSize: '12px',  marginRight: '3px', marginTop: '2px'  }}></i>
                     <p className='text-xs'>{categoryName}</p>
+                    
                 </div>
-
+               
             </div>
-
-        </ProductGrid>
+            <span className='text-xs ml-1'>TIMH:</span>
+            <span className='text-xs ml-2'>{PRICER},00$</span>
+        </ProductBasket>
     )
 }
 
 
 
+const ContainerBasket = styled.div`
+   
+
+    .basket-icon {
+        display: flex;
+        align-items: center;
+
+    }
+
+`
+
+
+const ProductBasket = styled.div`
+    .details {
+        display: flex;
+        align-items: center;
+        div {
+            display: flex;
+            margin-top: 2px;
+            margin-bottom: 10px;
+            margin-right: 10px;
+        }
+    }
+`
 
 
 
