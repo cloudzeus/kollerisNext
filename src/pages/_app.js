48f5c9df9635023cebf9ad76257225fdd4f554b8
giannischiout <giannis.chiout@gmail.com>
2023-10-03 21:20:57 +0300

    test 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,14 +6,14 @@ import { SessionProvider } from "next-auth/react"
 import { store } from '@/store';
 import theme from '@/theme/theme';
 import { ThemeProvider } from 'styled-components';
-function App({ Component, pageProps: { session, ...rest } }) {
+function App({ Component, pageProps: {session, pageProps} }) {
     // const {store, props} = wrapper.useWrappedStore(rest);
 
     return (
         <Provider store={store}>
         <SessionProvider session={session}>
             <ThemeProvider theme={theme}>
-                <Component {...props} />
+            <Component {...pageProps} />
             </ThemeProvider>
         </SessionProvider>
         </Provider>
