d0ac4564feac549f2d9fcafb40f17547df3a362a
giannischiout <giannis.chiout@gmail.com>
2023-10-06 20:45:41 +0300

    endpoints2

--- src/pages/api/alterProduct.js
+++ src/pages/api/alterProduct.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     let data = req.body;
     try {
-        return res.status(200).json({ success: true, result: data, message: "My milkshake brings all the boys to the yard And they're like, it's better than yours Damn right, it's better than yours I can teach you, but I have to charge My milkshake brings all the boys to the yard And they're like, it's better than yours Damn right, it's better than yours I can teach you, but I have to charge" })
+        return res.status(200).json({ success: true, result: data, message: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you" })
     } catch (e) {
         return res.status(500).json({ success: false, result: null })
     }
