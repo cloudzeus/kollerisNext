40e577d542a8bc8cc78c8013b8502646bea8b664
giannischiout <giannis.chiout@gmail.com>
2023-10-07 20:44:02 +0300

    cron

--- src/pages/api/alterAvailability.js
+++ src/pages/api/alterAvailability.js
@@ -1,32 +1,32 @@
 import { connect } from "mongoose";
 import SoftoneProduct from "../../../server/models/newProductModel";
-// export default async function handler(req, res) {
-//     let data = req.body;
-//     const now = new Date();
-//     const formattedDateTime = format(now, 'yyyy-MM-dd HH:mm:ss');
-//     let updateResults = [];
+export default async function handler(req, res) {
+    let data = req.body;
+    const now = new Date();
+    const formattedDateTime = format(now, 'yyyy-MM-dd HH:mm:ss');
+    let updateResults = [];
 
-//     try {
-//         await connectMongo();
-//         for (let item of data) {
-//             let update = await SoftoneProduct.updateOne({ MTRL: item.MTRL }, {
-//                 $set: {
-//                     availability: {
-//                         DIATHESIMA: item.DIATHESIMA,
-//                         SEPARAGELIA: item.SEPARAGELIA,
-//                         DESVMEVMENA: item.DESVMEVMENA,
-//                         date: formattedDateTime.toString()
-//                     }
-//                 }
-//             })
-//             updateResults.push({
-//                 MTRL: item.MTRL,
-//                 updated: update.nModified > 0 
-//             });
+    try {
+        await connectMongo();
+        for (let item of data) {
+            let update = await SoftoneProduct.updateOne({ MTRL: item.MTRL }, {
+                $set: {
+                    availability: {
+                        DIATHESIMA: item.DIATHESIMA,
+                        SEPARAGELIA: item.SEPARAGELIA,
+                        DESVMEVMENA: item.DESVMEVMENA,
+                        date: formattedDateTime.toString()
+                    }
+                }
+            })
+            updateResults.push({
+                MTRL: item.MTRL,
+                updated: update.nModified > 0 
+            });
     
-//         }
-//         return res.status(200).json({ success: true, result: updateResults  })
-//     } catch (e) {
-//         return res.status(500).json({ success: false, result: null })
-//     }
-// }
+        }
+        return res.status(200).json({ success: true, result: updateResults  })
+    } catch (e) {
+        return res.status(500).json({ success: false, result: null })
+    }
+}
