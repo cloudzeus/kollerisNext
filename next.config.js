cd7e347927bd74a296b8b3bff77839dde38cfde8
giannischiout <giannis.chiout@gmail.com>
2023-10-07 11:32:49 +0300

    test2

--- next.config.js
+++ next.config.js
@@ -4,27 +4,15 @@ const nextConfig = {
  
 }
 
-
 module.exports = {
   env: {
     MONGO_URI: process.env.MONGO_URI,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    BASE_URL: process.env.BASE_URL,
-    SECRET_KEY: process.env.SECRET_KEY,
-    NODEMAILER_EMAIL: process.env.NODEMAILER_EMAIL,
-    NODEMAILER_PASS: process.env.NODEMAILER_PASS,
-    URL: process.env.URL,
-    SERIAL_NO: process.env.SERIAL_NO,
-    DOMAIN: process.env.DOMAIN,
-    PROTOCOL: process.env.PROTOCOL,
-    USER_NAME: process.env.USER_NAME,
-    PASSWORD: process.env.PASSWORD,
-    APP_ID: process.env.APP_ID,
-    COMPANY: process.env.COMPANY,
-    MODULE: process.env.MODULE,
-    REFID: process.env.REFID,
-    VERSION: process.env.VERSION,
-    NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_SECRET_KEY: process.env.NEXT_PUBLIC_SECRET_KEY,
+    NEXT_PUBLIC_NODEMAILER_EMAIL: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
+    NEXT_PUBLIC_NODEMAILER_PASS: process.env.NEXT_PUBLIC_NODEMAILER_PASS,
+    NEXT_PUBLIC_SOFTONE_URL: process.env.NEXT_PUBLIC_SOFTONE_URL,
   },
 };
 
