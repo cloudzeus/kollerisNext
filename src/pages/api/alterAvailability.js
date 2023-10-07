9c7728604a90fcc0d96cdbfd2be04e443e633b53
giannischiout <giannis.chiout@gmail.com>
2023-10-07 21:45:29 +0300

    api

--- src/pages/api/alterAvailability.js
+++ src/pages/api/alterAvailability.js
@@ -5,29 +5,25 @@ export default async function handler(req, res) {
     let data = req.body;
     const now = new Date();
     const formattedDateTime = format(now, 'yyyy-MM-dd HH:mm:ss');
-    let updateResults = [];
     try {
         await connectMongo();
-        for (let item of data) {
-            console.log(item)
-            let update = await SoftoneProduct.updateOne({ MTRL: item.MTRL }, {
+        let update = await SoftoneProduct.updateOne({ MTRL: data.MTRL }, {
                 $set: {
                     availability: {
-                        DIATHESIMA: item.DIATHESIMA,
-                        SEPARAGELIA: item.SEPARAGELIA,
-                        DESVMEVMENA: item.DESVMEVMENA,
+                        DIATHESIMA: data.DIATHESIMA,
+                        SEPARAGELIA: data.SEPARAGELIA,
+                        DESVMEVMENA: data.DESVMEVMENA,
                         date: formattedDateTime.toString()
                     }
                 }
             })
-            console.log(update)
-            updateResults.push({
+           
+             let updateResult = {
                 MTRL: item.MTRL,
                 updated: update.modifiedCount > 0 
-            });
-            console.log(updateResults)
-        }
-        return res.status(200).json({ success: true, result: updateResults  })
+            };
+            return res.status(200).json({ success: true, result: updateResult  })
+      
     } catch (e) {
         return res.status(500).json({ success: false, result: null })
     }
