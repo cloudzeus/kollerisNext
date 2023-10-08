aa18cc013476c80233c72a6b765c9dc5d11db5b6
giannischiout <giannis.chiout@gmail.com>
2023-10-08 15:47:45 +0300

    next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
- 
+  images: {
+    domains: ['next.mbmv.io'],
+  },
 }
 
 module.exports = {
+  
   env: {
     MONGO_URI: process.env.MONGO_URI,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
