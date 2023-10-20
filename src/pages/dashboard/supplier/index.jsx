8311c13023cab6a9938cc107514ad79e17efe1f3
giannischiout <giannis.chiout@gmail.com>
2023-10-20 10:50:41 +0300

    error

---
+++ src/pages/dashboard/supplier/index.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "@/layouts/Admin/AdminLayout";
+const Page = () => {
+
+    return (
+        <AdminLayout>
+            
+        </AdminLayout >
+    )
+}
+
+export default Page;
