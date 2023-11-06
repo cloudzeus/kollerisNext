e955213d391139337f4c16f207d628cf7a931fe2
giannischiout <giannis.chiout@gmail.com>
2023-11-06 10:29:22 +0200

    correct upload to bunny cdn

--- src/pages/api/storageZone.js
+++ src/pages/api/storageZone.js
@@ -1,56 +1,37 @@
-import formidable from 'formidable';
-import fs from 'fs';
-import https from 'https';
 
+import axios from 'axios';
 
-
-const REGION = '';
+const REGION = 'storage'; // If German region, set this to an empty string: ''
 const BASE_HOSTNAME = 'storage.bunnycdn.com';
 const HOSTNAME = REGION ? `${REGION}.${BASE_HOSTNAME}` : BASE_HOSTNAME;
 const STORAGE_ZONE_NAME = 'kolleris';
+const FILENAME_TO_UPLOAD = 'filenameyouwishtouse.img';
+const FILE_PATH = '/images';
 const ACCESS_KEY = 'd4374cd3-86c0-4943-903e419f73de-008e-4a66';
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
-const uploadFileToBunnyCDN = async (file) => {
-  const filename = 'uploadedFile.txt'; // Modify this to your desired filename on BunnyCDN
+const fileName = 'test02.jpg'
+const storageZoneName = 'kolleris'
+const region = 'storage'
+const path = 'images'
 
-  const options = {
-    method: 'PUT',
-    host: HOSTNAME,
-    path: `/${STORAGE_ZONE_NAME}/${filename}`,
-    headers: {
-      AccessKey: ACCESS_KEY,
-      'Content-Type': 'application/octet-stream',
-    },
-  };
-
-  const req = https.request(options, (res) => {
-    res.on('data', (chunk) => {
-      console.log(chunk.toString('utf8'));
-    });
-  });
-
-  const readStream = fs.createReadStream(file.path);
-  readStream.pipe(req);
-};
 
 export default async function handler(req, res) {
-  const form = new formidable.IncomingForm();
+const data = req.body;
+console.log('file')
+console.log(data)
 
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      console.error('Error parsing form:', err);
-      res.status(400).end();
-      return;
-    }
 
-    const file = files.file;
-    uploadFileToBunnyCDN(file);
-    res.status(201).end();
-  });
+// const headers = {
+//   AccessKey: ACCESS_KEY,
+//   'Content-Type': 'application/octet-stream',
+// }
+  
+//  let result = await axios.put(`https://${region}.bunnycdn.com/${storageZoneName}/${fileName}`, data , { headers: headers })
+//  console.log('result bunny cdn')
+//  console.log(result.data)
+//  return res.status(200).json({ result: result.data })
+
+
+
 }
--- src/pages/dashboard/testUpload.js
+++ src/pages/dashboard/testUpload.js
@@ -1,53 +1,55 @@
 import axios from 'axios';
-import { set } from 'mongoose';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+const ACCESS_KEY = 'd4374cd3-86c0-4943-903e419f73de-008e-4a66';
+
+const fileName = 'test05.jpg'
+const storageZoneName = 'kolleris'
+const region = 'storage'
+const path = 'images'
+const headers = {
+  AccessKey: ACCESS_KEY,
+  'Content-Type': 'application/octet-stream',
+}
+
 
 const TestUpload = () => {
     const [binaryData, setBinaryData] = useState(null)
     const [selectedFile, setSelectedFile] = useState(null)
     const onUpload = async (event) => {
         const file = event.target.files[0];
-        console.log(file)
-        setSelectedFile(file)
+        const reader = new FileReader();
+            reader.onload = (event) => {
+                const arrayBuffer = event.target.result;
+                setBinaryData(arrayBuffer);
+            };
+        reader.readAsArrayBuffer(file);
+
     }
 
+
+    useEffect(() => {
+        console.log('binaryData')
+        console.log(binaryData)
+    }, [binaryData])
+
+    
     const onSubmit = async () => {
-        if (!selectedFile) return;
-      
-        const reader = new FileReader();
-      
-        reader.onload = async () => {
-          try {
-            const arrayBuffer = reader.result;
-            const hash = await crypto.subtle.digest('SHA-256', new Uint8Array(arrayBuffer));
-            const hashString = Array.from(new Uint8Array(hash))
-              .map((byte) => byte.toString(16).padStart(2, '0'))
-              .join('')
-              .toUpperCase();
-      
-            const formData = new FormData();
-            formData.append('file', selectedFile);
-            formData.append('hash', hashString);
-      
-            try {
-              const response = await axios.post('/api/storageZone', formData);
-              console.log('Backend response:', response.data);
-            } catch (error) {
-              console.error('Error:', error);
-            }
-          } catch (error) {
-            console.error('Error calculating hash:', error);
-          }
-        };
-      
-        reader.readAsArrayBuffer(selectedFile);
-      };
+        try {
+            if (!binaryData) return;
+            console.log(binaryData)
+             let result = await axios.put(`https://${region}.bunnycdn.com/${storageZoneName}/${fileName}`, binaryData , { headers: headers })
+             console.log(result.data)
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
     return (
         <div className='p-2'>
             <label htmlFor="avatar">Choose a profile picture:</label>
             <input type="file" id="avatar" name="avatar" onChange={onUpload} />
             <div>
-                <button  onClick={onSubmit}>Submit</button>
+                <button disabled={!binaryData} onClick={onSubmit}>Submit</button>
             </div>
         </div>
     )
