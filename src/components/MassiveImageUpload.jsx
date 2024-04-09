81cf53a6892eb783680ff62ee4088a971a141de3
giannischiout <giannis.chiout@gmail.com>
2024-04-09 09:59:20 +0300

    ui: add message to Images excell being empty

--- src/components/MassiveImageUpload.jsx
+++ src/components/MassiveImageUpload.jsx
@@ -5,15 +5,16 @@ import { useRouter } from 'next/router';
 import { Button } from 'primereact/button';
 import { useDispatch } from 'react-redux';
 import { setGridData } from '@/features/catalogSlice';
+import { Toast } from 'primereact/toast';
 
 const MassiveImageUpload = () => {
     const fileInputRef = useRef(null);
+    const toast = useRef(null);
     const router = useRouter();
     const dispatch = useDispatch();
     const [fileLoading, setFileLoading] = useState(false)
     const handleFileUpload = async (e) => {
         setFileLoading(true)
-
         const reader = new FileReader();
         reader.readAsArrayBuffer(e.target.files[0]);
         reader.onload = async (e) => {
@@ -32,16 +33,26 @@ const MassiveImageUpload = () => {
                 dispatch(setHeaders(headers))
                 setFileLoading(false)
                 router.push('/dashboard/product/multi-image-upload')
+            } else {
+                setFileLoading(false)
+                showWarn()
+                return;
             }
+              
         };
     };
 
+    const showWarn = () => {
+        toast.current.show({severity:'warn', summary: 'Warning', detail:'Δεν βρέθηκαν δεδομένα στο αρχείο', life: 3000});
+    }
     const onUploadClick = () => {
         fileInputRef.current.click()
     }
 
+   
     return (
         <div>
+            <Toast ref={toast} />
             <input hidden className="hide" ref={fileInputRef} type="file" onChange={(e) => handleFileUpload(e)} />
             <Button className='w-full' severity='warning' loading={fileLoading} onClick={onUploadClick} label="Ανέβασμα Φωτογραφιών xlsx" icon="pi pi-upload"></Button>
         </div>
