2da7f9722801dfc2ee02216b5cabc50a4877bc6e
giannischiout <giannis.chiout@gmail.com>
2024-04-26 09:48:43 +0300

    deploy test

--- src/pages/auth/thankyouregistration.js
+++ src/pages/auth/thankyouregistration.js
@@ -1,6 +1,5 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
 import { BasicContainer } from '@/components/styles'
